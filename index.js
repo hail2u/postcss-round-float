e7fe2f7cf03c0c09b725a10cf394bc9fc64a5954
Kyo Nagashima <hail2u@gmail.com>
2016-05-25 12:40:09 +0900

    Enable passing place number by the user

--- index.js
+++ index.js
@@ -11,16 +11,24 @@ function roundFloat(p, f) {
     return f;
   }
 
+  if (p === 0) {
+    return Math.round(f);
+  }
+
   p = Math.pow(10, p);
 
-  return Math.round(parseFloat(f) * p) / p;
+  return Math.round(f * p) / p;
 }
 
-module.exports = postcss.plugin("round-float", function () {
+module.exports = postcss.plugin("round-float", function (place) {
+  if (!Number.isInteger(place) || place < 0) {
+    place = 3;
+  }
+
   return function (css) {
     css.walkDecls(function (decl) {
       decl.value = postcss.list.comma(decl.value).map(function (value) {
-        return value.replace(reFloat, roundFloat.bind(null, 3));
+        return value.replace(reFloat, roundFloat.bind(null, place));
       }).join(",");
     });
   };
